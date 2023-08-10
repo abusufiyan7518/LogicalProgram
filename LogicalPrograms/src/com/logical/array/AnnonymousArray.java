7a4beb31dc9d417f93ad61ef40a32ac888ff102d
Abusufiyan <140253620+abusufiyan7518@users.noreply.github.com>
2023-08-11 01:11:01 +0530

    Update AnnonymousArray.java

--- LogicalPrograms/src/com/logical/array/AnnonymousArray.java
+++ LogicalPrograms/src/com/logical/array/AnnonymousArray.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class AnnonymousArray {
   public static void main(String[] args) {
 	System.out.println(Arrays.toString(new int [] { 1,23,4,67,2}));
+	 System.out.println(Arrays.toString(new int [] { 1,23,4,67,2}));  
 	
 }
 }
