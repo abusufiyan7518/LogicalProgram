68426d3e947434cca14661d2c50ae0e6e01ca0a5
91751 <91751@LAPTOP-A9N4KV2V>
2023-08-11 01:08:43 +0530

    new

---
+++ LogicalPrograms/src/com/logical/array/AnnonymousArray.java
@@ -0,0 +1,10 @@
+package com.logical.array;
+
+import java.util.Arrays;
+
+public class AnnonymousArray {
+  public static void main(String[] args) {
+	System.out.println(Arrays.toString(new int [] { 1,23,4,67,2}));
+	
+}
+}
