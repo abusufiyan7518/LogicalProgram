723816e1899b3cba302dc48334853c102785cb3d
91751 <91751@LAPTOP-A9N4KV2V>
2023-08-12 16:14:52 +0530

    .

--- LogicalPrograms/src/com/logical/array/AnnonymousArray.java
+++ LogicalPrograms/src/com/logical/array/AnnonymousArray.java
@@ -6,8 +6,6 @@ public class AnnonymousArray {
   public static void main(String[] args) {
 	System.out.println(Arrays.toString(new int [] { 1,23,4,67,2}));
 	 System.out.println(Arrays.toString(new int [] { 1,23,4,67,2}));  
-	System.out.println(Arrays.toString(new int [] { 1,23,4,67,2}));
-	 System.out.println(Arrays.toString(new int [] { 1,23,4,67,2}));  
-	
+
 }
 }
